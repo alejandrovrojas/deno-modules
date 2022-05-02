a414b5fbc5083bc9a25b5556453c11455599db5b
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-02 20:13:34 +0200

    Reorder dependencies

--- dependencies.ts
+++ dependencies.ts
@@ -1,8 +1,8 @@
 import { Application, Router, Status, Context, Request, Response } from 'https://deno.land/x/oak/mod.ts';
 import { render } from 'https://deno.land/x/nano/mod.ts';
 import { lookup as mimetype } from 'https://deno.land/x/media_types/mod.ts';
-import { parse as parse_flags } from 'https://deno.land/std/flags/mod.ts';
 import { debounce } from 'https://deno.land/std/async/mod.ts';
+import { parse as parse_flags } from 'https://deno.land/std/flags/mod.ts';
 import { ensureDir as ensure_dir, ensureFile as ensure_file } from 'https://deno.land/std/fs/mod.ts';
 
 const process = parse_flags(Deno.args);
