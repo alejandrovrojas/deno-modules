2481279b6f469c945a454d71a5418c47d85775c2
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-15 02:01:45 +0200

    Update nano

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 import { Application, Router, Status, Context } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
-import { render } from 'https://deno.land/x/nano@v0.0.8/mod.ts';
+import { render } from 'https://deno.land/x/nano@v0.0.9/mod.ts';
 import { debounce } from 'https://deno.land/std@0.137.0/async/mod.ts';
 import { join as path_join, parse as path_parse } from 'https://deno.land/std@0.137.0/path/mod.ts';
 import { parse as flags_parse } from 'https://deno.land/std@0.137.0/flags/mod.ts';
