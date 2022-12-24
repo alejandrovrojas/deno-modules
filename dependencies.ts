27f3f8082f67ccee04a0f25907394574294f861b
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-12-24 09:20:20 +0100

    Import latest nano

--- dependencies.ts
+++ dependencies.ts
@@ -1,4 +1,4 @@
-import nano from "https://deno.land/x/nano@1.0.1/mod.ts";
+import nano from "https://deno.land/x/nano/mod.ts";
 import { debounce } from 'https://deno.land/std@0.165.0/async/mod.ts';
 import { parse as parse_flags } from 'https://deno.land/std@0.165.0/flags/mod.ts';
 import { join as join_path } from 'https://deno.land/std@0.165.0/path/mod.ts';
