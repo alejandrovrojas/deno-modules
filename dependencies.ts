246786ea25c36484672b1d4b9bbf78098001b829
Alejandro V. Rojas <6848068+alejandrovrojas@users.noreply.github.com>
2023-12-26 13:32:16 +0100

    Bump std version

--- dependencies.ts
+++ dependencies.ts
@@ -1,7 +1,8 @@
 import nano from 'https://deno.land/x/nano/mod.ts';
-import { debounce } from 'https://deno.land/std@0.195.0/async/mod.ts';
-import { parse as parse_flags } from 'https://deno.land/std@0.195.0/flags/mod.ts';
-import { join as join_path } from 'https://deno.land/std@0.195.0/path/mod.ts';
-import { serveDir as serve_dir } from 'https://deno.land/std@0.195.0/http/file_server.ts';
+import { debounce } from 'https://deno.land/std@0.210.0/async/mod.ts';
+import { parse as parse_flags } from 'https://deno.land/std@0.210.0/flags/mod.ts';
+import { join as join_path } from 'https://deno.land/std@0.210.0/path/mod.ts';
+import { exists as path_exists } from 'https://deno.land/std@0.210.0/fs/mod.ts';
+import { serveDir as serve_dir } from 'https://deno.land/std@0.210.0/http/file_server.ts';
 
-export { nano, debounce, parse_flags, join_path, serve_dir };
+export { nano, debounce, parse_flags, join_path, path_exists, serve_dir };
